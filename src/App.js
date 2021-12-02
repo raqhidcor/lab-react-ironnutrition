be77becd125c873ef6b52338ff859aad41ad0ecb
Raquel <90280742+raqhidcor@users.noreply.github.com>
2021-12-02 13:34:45 +0100

    Iteration 2 completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { useState } from "react";
 import { Row, Divider, Button } from "antd";
 import foods from "./foods.json";
+import FoodBox from "./Components/FoodBox";
 
 function App() {
   const [foodList, setFoodList] = useState(foods);
@@ -15,19 +16,17 @@ function App() {
       {/* Display Search component here */}
 
       <Divider>Food List</Divider>
-      
+
       <Row style={{ width: "100%", justifyContent: "center" }}>
-        
-      {foodList.map((foods, index) => (
-      <div>
-        <p> {foods.name} </p>
-        <img src={foods.image} alt={foods.name} width={100} />
-      </div>
-      ))}
-        
+        {foodList.map((foods, index) => {
+          console.log(foodList)
+          return (
+            <div key={index}>
+              <FoodBox foods={foods}/>
+            </div>
+          );
+        })}
       </Row>
-
-
     </div>
   );
 }
---
+++ src/Components/FoodBox.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Card, Col, Divider, Button } from "antd";
+
+function FoodBox(props) {
+    const {name,calories,image,servings} = props.foods
+    return (
+      <Col>
+        <Card title={name} style={{ width: 230, height: 300, margin: 10 }} >
+          <img src={image} height={60} alt= {name} />
+          <p>Calories: {calories}</p>
+          <p>Servings: {servings}</p>
+          <p>
+            <b>Total Calories: {calories * servings} </b> kcal
+          </p>
+          <Button type="primary"> Delete </Button>
+        </Card>
+      </Col>
+    );
+  }
+  
+  export default FoodBox;
+  
+  
